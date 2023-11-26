9d40c23b9e0a24443f8bd80f8ce50fbe08adfca5
Envoy49 <94023356+Envoy49@users.noreply.github.com>
2023-11-26 23:02:31 +1100

    use my own formula

---
+++ go-spotify-cli.rb
@@ -0,0 +1,17 @@
+class GoSpotifyCli < Formula
+  desc "A CLI tool for interacting with Spotify"
+  homepage "https://github.com/Envoy49/go-spotify-cli"
+  url "https://github.com/Envoy49/go-spotify-cli/archive/v1.0.0.tar.gz"
+  sha256 "22f3592b5a6bffdf306c10cd9fe216ef01fdcf8a1c3256928e61026ef26d06d3"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", "go-spotify-cli"
+    bin.install "go-spotify-cli"
+  end
+
+  test do
+    system "#{bin}/go-spotify-cli", "--version"
+  end
+end
