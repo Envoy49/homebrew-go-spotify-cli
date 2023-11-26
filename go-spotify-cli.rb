b9b596bed3aab4859fe9a638f20a491455e94568
Envoy49 <94023356+Envoy49@users.noreply.github.com>
2023-11-26 23:05:33 +1100

    try different url

--- go-spotify-cli.rb
+++ go-spotify-cli.rb
@@ -1,7 +1,7 @@
 class GoSpotifyCli < Formula
   desc "A CLI tool for interacting with Spotify"
   homepage "https://github.com/Envoy49/go-spotify-cli"
-  url "https://github.com/Envoy49/go-spotify-cli/archive/v1.0.0.tar.gz"
+  url "https://github.com/Envoy49/go-spotify-cli/archive/refs/tags/v1.0.0.tar.gz"
   sha256 "22f3592b5a6bffdf306c10cd9fe216ef01fdcf8a1c3256928e61026ef26d06d3"
 
   depends_on "go" => :build
