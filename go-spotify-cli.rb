105f0b18f23c2f9ac30743a4c146fa1a9ab31779
GitHub Action <action@github.com>
2024-01-07 22:11:03 +0000

    Update go-spotify-cli formula to version v1.0.64

--- go-spotify-cli.rb
+++ go-spotify-cli.rb
@@ -1,8 +1,8 @@
 class GoSpotifyCli < Formula
   desc "A CLI tool for interacting with Spotify"
   homepage "https://github.com/Envoy49/go-spotify-cli"
-  url "https://github.com/Envoy49/go-spotify-cli/archive/refs/tags/v1.0.63.tar.gz"
-  sha256 "11dc7facc6ffe687409e13bb4e15aeb51d78ddca9635d003d0d148ebf8381207"
+  url "https://github.com/Envoy49/go-spotify-cli/archive/refs/tags/v1.0.64.tar.gz"
+  sha256 "2e03c8fd2f9272f75afa4b74c514c95815d7ac2a4f2d25f518e039730cb8360e"
 
   depends_on "go" => :build
 
